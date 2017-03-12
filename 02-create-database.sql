115e7ee1e63fa6a4458fd7390fb1d90f2f06f0bd
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-12 08:47:42 -0700

    Basic invoice

--- 02-create-database.sql
+++ 02-create-database.sql
@@ -1,4 +1,27 @@
-create table if not exists a_table(
-  id uuid default uuid_generate_v4(),
-  constraint a_table_pk primary key(id)
+create table if not exists invoice(
+  id uuid DEFAULT uuid_generate_v4(),
+  invoice_date date not null,
+  message text,
+  description text,
+  CONSTRAINT _pk PRIMARY key(id)
+);
+
+create table if not exists invoice_item_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT invoice_item_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT invoice_item_type_pk PRIMARY key(id)
+);
+
+create table if not exists invoice_item(
+  id uuid DEFAULT uuid_generate_v4(),
+  taxable_flag boolean not null default true,
+  amount double precision,
+  item_description text,
+  adjustment_for uuid not null references invoice(id),
+  sold_for uuid not null references invoice(id),
+  part_of_invoice_id uuid not null,
+  charge_for_product_feautre uuid not null,
+  charge_for_product uuid not null,
+  described_by uuid not null references invoice_item_type(id),
+  CONSTRAINT invoice_item_pk PRIMARY key(id)
 );
