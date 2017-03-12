01fb8a1190e0beee4bbfb2cf5fda987dddeb49b7
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-12 09:05:39 -0700

    Invoice items

--- 02-create-database.sql
+++ 02-create-database.sql
@@ -12,16 +12,27 @@ create table if not exists invoice_item_type(
   CONSTRAINT invoice_item_type_pk PRIMARY key(id)
 );
 
+create table if not exists adjustment_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT adjustment_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT adjustment_type_pk PRIMARY key(id)
+);
 create table if not exists invoice_item(
   id uuid DEFAULT uuid_generate_v4(),
   taxable_flag boolean not null default true,
   amount double precision,
   item_description text,
+  percentage double precision,
   adjustment_for uuid not null references invoice(id),
-  sold_for uuid not null references invoice(id),
+  sold_for_invoice uuid not null references invoice(id),
   part_of_invoice_id uuid not null,
   charge_for_product_feautre uuid not null,
   charge_for_product uuid not null,
   described_by uuid not null references invoice_item_type(id),
+  adjustment_for_invoice_item uuid not null references invoice_item_type(id),
+  sold_for_invoice_item uuid references invoice_item(id),
+  charge_for_product_feature uuid,
+  charge_for_prodcut uuid,
+  adjustment_type_id uuid references adjustment_type(id),
   CONSTRAINT invoice_item_pk PRIMARY key(id)
 );
---
+++ 03-initial-data.sql
@@ -0,0 +1,14 @@
+insert into invoice_item_type (description) values ('Sales Invoice Item');
+insert into invoice_item_type (description) values ('Purchase Invoice Item');
+insert into invoice_item_type (description) values ('Invoice Adjustment');
+insert into invoice_item_type (description) values ('Invoice Adjustment');
+insert into invoice_item_type (description) values ('Invoice Discount Adjustment');
+insert into invoice_item_type (description) values ('Invoice Surcharge Adjustment');
+insert into invoice_item_type (description) values ('Invoice Sales Tax');
+insert into invoice_item_type (description) values ('Invoice Shipping and Handling Charge');
+insert into invoice_item_type (description) values ('Invoice Fee');
+insert into invoice_item_type (description) values ('Invoice Acquiring Item');
+insert into invoice_item_type (description) values ('Invoice Product Feature Item');
+insert into invoice_item_type (description) values ('Invoice Product Item');
+insert into invoice_item_type (description) values ('Invoice Work Effort Item');
+insert into invoice_item_type (description) values ('Invoice Time Entry Item');
